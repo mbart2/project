9d6099ce8017fdd438c35f798db3b4f2a41e5fbd
Matthew Barteau <bart5@umbc.edu>
2014-06-04 19:37:24 -0400

    fdsa

--- testing.c
+++ testing.c
@@ -2,6 +2,17 @@
 
 int main(){
 	printf("hi\n");
+<<<<<<< HEAD
+<<<<<<< HEAD
+	printf("third\n");
+=======
+	printf("23\n");
+>>>>>>> e8b1155... second
+=======
 	printf("wha sec af t is up\n");
+>>>>>>> 3fb7070... first
+=======
+	printf("wha sec af t is up\n");
+>>>>>>> 3fb7070... first
 	return 0;
 }
